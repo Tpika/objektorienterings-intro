f0dc18318baa23d21cefa53949611c31d5f81bde
Tpika <pikachumio4@gmail.com>
2021-10-27 14:44:54 +0200

    Escape

--- bilregister.py
+++ bilregister.py
@@ -24,12 +24,34 @@ class Car():
         så tilldelas aktuellt objekt märket enligt parametern.
         '''
         self.brand = new_brand
+    
+    def set_color(self, new_color):
+        self.color = new_color
+        print(self.color)
 
+    def set_mileage(self, new_mileage):
+        self.mileage = new_mileage
+        print(self.mileage)
+
+    def get_mileage(self):
+        a = self.mileage
+        return a
 
 # ----------Huvudprogram----------
 # Nu när klassen finns kan vi skapa objekt (variabler) med denna typ.
 # Dessa objekt har också tillgång till klassens metoder (funktioner).
 a_car = Car('Volvo', 'Blå', 1587)
 a_car.get_brand()
-a_car.set_brand('Renault')
-a_car.get_brand()
+a_car.set_color('Blå')
+a_car.set_mileage(12000)
+
+b_car = Car('Lambourgini', 'Rosa', 10)
+b_car.get_brand()
+b_car.set_color('Rosa')
+b_car.set_mileage(1000)
+
+cars = [a_car, b_car]
+
+for car in cars:
+    print(car.brand)
+
